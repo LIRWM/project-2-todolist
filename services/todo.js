400db910ac82926034c94e980d842c2622f3d47b
LIRWM <a.isslamia@gmail.com>
2025-06-21 19:33:23 +0300

    fix todo.js

--- services/todo.js
+++ services/todo.js
@@ -1,12 +1,47 @@
 class TodoService {
     constructor() {
-        this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-        this.archivedTodos = JSON.parse(localStorage.getItem('archivedTodos')) || [];
+        this.initializeData();
+    }
+    initializeData() {
+        const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser?.email) {
+            this.todos = JSON.parse(localStorage.getItem(`todos_${currentUser.email}`)) || [];
+            this.archivedTodos = JSON.parse(localStorage.getItem(`archivedTodos_${currentUser.email}`)) || [];
+        } else {
+            this.todos = [];
+            this.archivedTodos = [];
+        }
     }
 
     async getAllTodos() {
         // Получение списка задач с сервера
+        this.initializeData();
         return this.todos;
+    }
+
+    async getArchivedTodos() {
+        this.initializeData();
+        return this.archivedTodos;
+    }
+
+    _getCurrentUserEmail() {
+        const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        return currentUser?.email;
+    }
+
+    _saveTodos() {
+        // Сохранение списка задач в локальное хранилище
+        const userEmail = this._getCurrentUserEmail();
+        if (userEmail) {
+            localStorage.setItem(`todos_${userEmail}`, JSON.stringify(this.todos));
+        }
+    }
+
+    _saveArchivedTodos() {
+        const userEmail = this._getCurrentUserEmail();
+        if (userEmail) {
+            localStorage.setItem(`archivedTodos_${userEmail}`, JSON.stringify(this.archivedTodos));
+        }
     }
 
     async createTodo(todoData) {
@@ -42,27 +77,23 @@ class TodoService {
     }
 
     async archiveTodo(id) {
-        // Архивирование задачи
         const index = this.todos.findIndex(todo => todo.id === id);
         if (index === -1) throw new Error('Задача не найдена');
 
         const todoToArchive = this.todos[index];
-        this.archivedTodos.push({ ...todoToArchive, archivedAt: new Date().toISOString() });
-        this.todos.splice(index, 1);
+        const category = todoToArchive.categoryId ? 
+            categories.find(cat => cat.id === todoToArchive.categoryId)?.name : 
+            'Без категории';
 
+        this.archivedTodos.push({
+            ...todoToArchive,
+            category,
+            archiveDate: new Date().toISOString()
+        });
+        
+        this.todos.splice(index, 1);
         this._saveTodos();
         this._saveArchivedTodos();
-        
-    }
-
-
-    _saveTodos() {
-        // Сохранение списка задач в локальное хранилище
-        localStorage.setItem('todos', JSON.stringify(this.todos));
-    }
-
-    _saveArchivedTodos() {
-        localStorage.setItem('archivedTodos', JSON.stringify(this.archivedTodos));
     }
 }
 

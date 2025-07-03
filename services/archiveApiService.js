a6dcfd289c6e36270b368301aed22429adfdeb86
LIRWM <a.isslamia@gmail.com>
2025-07-04 00:26:16 +0300

    REST API: add archiveApiService.js

---
+++ services/archiveApiService.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'http://localhost:3000/archivedTodos';
+
+export  async function getArchivedTodos() {
+    const res = await fetch(BASE_URL);
+    return await res.json();
+}
+
+export  async function addToArchive(todo) {
+    const res = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(todo),
+    });
+    return await res.json();
+}
+
+export async function deleteToArchiveTodo(id) {
+    return await fetch(`${BASE_URL}/${id}`, {
+        method: 'DELETE',
+    });
+}

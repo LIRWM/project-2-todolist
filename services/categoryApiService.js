292dad7cb469d72fa5b64a95da5e5ddbedf654f9
LIRWM <a.isslamia@gmail.com>
2025-07-04 00:07:23 +0300

    REST API: add categoryApiService.js

---
+++ services/categoryApiService.js
@@ -0,0 +1,30 @@
+const BASE_URL = 'http://localhost:3000/categories';
+
+export  async function getCategories() {
+    const res = await fetch(BASE_URL);
+    return await res.json();
+}
+
+export  async function addCategory(category) {
+    const res = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(category),
+    });
+    return await res.json();
+}
+
+export async function deleteCategory(id) {
+    return await fetch(`${BASE_URL}/${id}`, {
+        method: 'DELETE',
+    });
+}
+
+export async function updateCategory(id, updates) {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(updates),
+    });
+    return await res.json();
+}
